4d27f05a525105760f5965e8bd16c08a2be4cf33
형언 <ilsin302a@gmail.com>
2023-11-21 17:31:26 +0900

    📝외박 관련 코드 작성

--- app/src/main/java/com/hs/dgsw/android/check/GoHomeActivity.kt
+++ app/src/main/java/com/hs/dgsw/android/check/GoHomeActivity.kt
@@ -8,9 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.hs.dgsw.android.check.databinding.ActivityGoHomeBinding
-import com.hs.dgsw.android.check.databinding.ActivityHomeBinding
+import android.app.DatePickerDialog
+import java.util.Calendar
 
 class GoHomeActivity : Fragment(), MainActivity.onBackPressedListener {
+
+    var returnDayData = ""
+    var passDayData = ""
     companion object{
         private const val TAG = "로그"
 
@@ -39,21 +43,31 @@ class GoHomeActivity : Fragment(), MainActivity.onBackPressedListener {
         val binding = ActivityGoHomeBinding.inflate(inflater)
 
         // 외박 날짜 누르면 캘린더 보여주기
-        binding.passDay.setOnClickListener{
-
+        binding.passDay.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val dateSetListener =
+                DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                    passDayData = "${year}년 ${month + 1}월 ${dayOfMonth}일"
+                }
+            DatePickerDialog(requireContext(), dateSetListener, cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH)).show()
+            binding.passDay.setText(passDayData)
         }
 
         // 복귀 날짜 누르면 캘린더 보여주기
         binding.returnDay.setOnClickListener{
-
+            val cal = Calendar.getInstance()
+            val dateSetListener =
+                DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                    returnDayData = "${year}년 ${month + 1}월 ${dayOfMonth}일"
+                }
+            DatePickerDialog(requireContext(), dateSetListener, cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH)).show()
+            binding.returnDay.setText(returnDayData)
         }
 
         // 외박 날짜, 복귀 날짜, 샤유, 서버로 보내기
         binding.sleepoverBtn.setOnClickListener{
 
         }
-
-
         return binding.root
     }
 
